ad3e96b2362f6f8102d9a35d724d0a30bf23642c
maxys146 <maxys146@gmail.com>
2022-09-18 23:06:53 +0300

    Закончил задание 1 по классам.

--- netology_5.03.01/netology_5.03.01.cpp
+++ netology_5.03.01/netology_5.03.01.cpp
@@ -5,8 +5,30 @@
 
 class Calculator
 {
-public:
+private:
     double num1, num2;
+public:
+    bool set_num1(double num1)
+    {
+        if (num1 != 0) {
+            this->num1 = num1;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    bool set_num2(double num2)
+    {
+        if (num2 != 0) {
+            this->num2 = num2;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
     double add()
     {
         return this->num1 + this->num2;
@@ -36,16 +58,37 @@ public:
 
 int main()
 {
+    setlocale(LC_ALL, "Russian");
+
+    int num1, num2;
     Calculator calc;
-    calc.num1 = 3;
-    calc.num2 = 6;
-    std::cout << "num1 + num2 = " << calc.add() << "\n";
-    std::cout << "num1 * num2 = " << calc.multiply() << "\n";
-    std::cout << "num1 - num2 = " << calc.subtract_1_2() << "\n";
-    std::cout << "num1 - num2 = " << calc.subtract_2_1() << "\n";
-    std::cout << "num1 / num2 = " << calc.divide_1_2() << "\n";
-    std::cout << "num1 / num2 = " << calc.divide_2_1() << "\n";
-    
+    while (true)
+    {
+        std::cout << "Введите число num1:";
+        std::cin >> num1;
+        while (!calc.set_num1(num1))
+        {
+            std::cout << "Неверный ввод!\n";
+            std::cout << "Введите число num1:";
+            std::cin >> num1;
+        }
+
+        std::cout << "Введите число num2:";
+        std::cin >> num2;
+        while (!calc.set_num2(num2))
+        {
+            std::cout << "Неверный ввод!\n";
+            std::cout << "Введите число num2:";
+            std::cin >> num2;
+        }
+
+        std::cout << "num1 + num2 = " << calc.add() << "\n";
+        std::cout << "num1 * num2 = " << calc.multiply() << "\n";
+        std::cout << "num1 - num2 = " << calc.subtract_1_2() << "\n";
+        std::cout << "num1 - num2 = " << calc.subtract_2_1() << "\n";
+        std::cout << "num1 / num2 = " << calc.divide_1_2() << "\n";
+        std::cout << "num1 / num2 = " << calc.divide_2_1() << "\n";
+    }
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
