fee025d4926fccae1193b8f4a5f8ea638599a739
maxys146 <maxys146@gmail.com>
2022-10-13 20:09:36 +0300

    Сделал "Задача 1. Фигуры. Количество сторон"

--- netology_5.05.01/netology_5.05.01.cpp
+++ netology_5.05.01/netology_5.05.01.cpp
@@ -3,9 +3,60 @@
 
 #include <iostream>
 
+class Figure
+{
+protected:
+    int sides_count;
+    std::string name;
+
+public:
+    Figure()
+    {
+        name = "Фигура";
+        sides_count = 0;
+    }
+    int get_sides_count()
+    {
+        return sides_count;
+    }
+    std::string get_name()
+    {
+        return name;
+    }
+};
+
+class Triangle : public Figure
+{
+public:
+    Triangle()
+    {
+        name = "Треугольник";
+        sides_count = 3;
+    }
+};
+
+class Quadrangle : public Figure
+{
+public:
+    Quadrangle()
+    {
+        name = "Четырехугольник";
+        sides_count = 4;
+    }
+};
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "Russian");
+
+    Figure figure;
+    Triangle traingle;
+    Quadrangle quadrangle;
+    std::cout << "Количество сторон: \n";
+    std::cout << figure.get_name() << ": " << figure.get_sides_count() << "\n";
+    std::cout << traingle.get_name() << ": " << traingle.get_sides_count() << "\n";
+    std::cout << quadrangle.get_name() << ": " << quadrangle.get_sides_count() << "\n";
+
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
