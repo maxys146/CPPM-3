a3d96703c8c77c3c291f896d38fd4945d77af116
maxys146 <maxys146@gmail.com>
2022-10-17 18:37:49 +0300

    Сделал задание 2 по макросам.

--- netology_5.07.02/netology_5.07.02.cpp
+++ netology_5.07.02/netology_5.07.02.cpp
@@ -2,10 +2,16 @@
 //
 
 #include <iostream>
+#define SUB(a, b) ((a) - (b))
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int a = 6;
+	int b = 5;
+	int c = 2;
+	std::cout << SUB(a, b) << std::endl;
+	std::cout << SUB(a, b) * c << std::endl;
+	std::cout << SUB(a, b + c) * c << std::endl;
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
