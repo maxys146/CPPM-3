5477acd8962d1381ca38fa4daf1b542c11108c2b
maxys146 <maxys146@gmail.com>
2022-10-31 22:45:21 +0300

    Добавил заготовку для родительского класса транспорта

---
+++ netology_5.racegame/Transport.cpp
@@ -0,0 +1,10 @@
+#include "Transport.h"
+
+bool Transport::setDistance(int distance)
+{
+	return true;
+}
+int Transport::getTotalRaceTime()
+{
+	return 1;
+}
---
+++ netology_5.racegame/Transport.h
@@ -0,0 +1,21 @@
+#pragma once
+class Transport
+{
+protected:
+    // Общие данные
+    int totalRestingTime = 0;
+    int restCount = 0;
+    int distance = 0;
+    int raceTimeWoRest = 0;
+    int totalRaceTime = 0;
+    // Данные для ТС
+    int speed = 0;
+    int timeToRest = 0;
+    int resting1stTime = 0;
+    int resting2ndTime = 0;
+    int restingOtherTime = 0;
+public:
+    bool setDistance(int distance);
+    int getTotalRaceTime();
+};
+
